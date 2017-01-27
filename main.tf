ed9c421e2d293f0f5fb6903abfdc61de3264f8ea
HashiCorp Training <training@hashicorp.com>
2017-01-27 17:56:13 +0000

    init

---
+++ main.tf
@@ -0,0 +1,69 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-7e50c21a
+#
+# Your security group ID is:
+#
+#     sg-29ef374e
+#
+# Your AMI ID is:
+#
+#     ami-30217250
+#
+# Your Identity is:
+#
+#     autodesk-dog
+#
+
+variable "aws_access_key" {
+  type = "string"
+
+  #  default = "AKIAI6J3BSESRNMYZAOQ"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+
+  #  default = "c2GgYeulJPMUAGFaAv5wNqjM+aMqFnxqHlOTTUEx"
+}
+
+variable "aws_region" {
+  type    = "string"
+  default = "us-west-1"
+}
+
+variable "num_webs" {
+  type    = "string"
+  default = "2"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-30217250"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-7e50c21a"
+  vpc_security_group_ids = ["sg-29ef374e"]
+  count                  = "${var.num_webs}"
+
+  tags {
+    Identity = "autodesk-dog"
+    Name     = "hyung-test ${count.index + 1}/${var.num_webs}"
+    Purpose  = "training"
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
